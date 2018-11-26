fc5e62d384ff63b17aa467dc71d8a5492cc9ef9b
andre1p <45165475+andre1p@users.noreply.github.com>
2018-11-26 11:51:29 +0100

    el build

---
+++ AWUG1/numbers/build/precache-manifest.9a90f506e2653d18b60527628b78727b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fef6f4c3a246acf1d755",
+    "url": "/static/js/main.fef6f4c3.chunk.js"
+  },
+  {
+    "revision": "cc5bf0703a7639dc11b1",
+    "url": "/static/js/1.cc5bf070.chunk.js"
+  },
+  {
+    "revision": "fef6f4c3a246acf1d755",
+    "url": "/static/css/main.9bc64946.chunk.css"
+  },
+  {
+    "revision": "3935d1a20104db09919ba684de48b211",
+    "url": "/index.html"
+  }
+];
