96b3bb8f20aeac923d102a5c1ce364930f274e02
andre1p <andreu.pc.1997@gmail.com>
2018-11-27 11:53:29 +0100

    Added basic scafold

--- AWUG1/fakkit/frontend/src/App.js
+++ AWUG1/fakkit/frontend/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import Header from "./Components/Header";
+import LinkList from "./Components/LinkList";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          fakkit
-        </header>
+          <Header />
+          <LinkList />
       </div>
     );
   }
